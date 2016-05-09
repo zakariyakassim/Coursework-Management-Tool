a288d9c5de90f92370a38987b0f0b4277520002a
Zakariya Kassim <zakariyakassim@eroam-144-87.wifi.wmin.ac.uk>
2016-05-09 16:31:54 +0100

    add coursework

--- Coursework Management Tool/ViewControllerAddCoursework.swift
+++ Coursework Management Tool/ViewControllerAddCoursework.swift
@@ -1,82 +1,53 @@
 //
-//  DetailViewController.swift
+//  ViewControllerMainMenu.swift
 //  Coursework Management Tool
 //
-//  Created by MACBOOKPRO on 20/03/2016.
+//  Created by MACBOOKPRO on 07/04/2016.
 //  Copyright © 2016 MACBOOKPRO. All rights reserved.
 //
 
 import UIKit
-import CoreData
 
-class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
-    
-   // var module_data = [NSManagedObject]()
-    
-    var placeHolderData: String!
-    var levels = ["Level 4", "Level 5", "Level 6", "Level 7"];
-    
-    var dataController = DataController()
-    
-    @IBOutlet weak var btnAddCoursework: MyButton!
+class ViewControllerAddCoursework: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+   // @IBOutlet weak var btnAddCoursework: MyButton!
     @IBOutlet weak var btnSave: MyButton!
     @IBOutlet weak var txtModule: UITextField!
     @IBOutlet weak var txtCode: UITextField!
     @IBOutlet weak var levelPickerView: UIPickerView!
     
-    var selectedLevel: String!
-    var position: Int?
-    
-    var table: UITableView?
-    var modules = [Module]()
-    
-    var detailItem: Module? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-    
-    func configureView() {
-        // Update the user interface for the detail item.
-        
-        if let detail = self.detailItem {
-            if let name = self.txtModule {
-                name.text = detail.name
-            }
-            if let code = self.txtCode {
-                code.text = String(position)
-            }
-        }
-    }
+    @IBOutlet weak var btnCancel: MyButton!
+        var selectedLevel: String!
+
+    var placeHolderData: String!
+    var levels = ["Level 4", "Level 5", "Level 6", "Level 7"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         self.customTextFieldStyle(txtModule, text: "Module Name")
-        self.customTextFieldStyle(txtCode, text: String(position))
+        self.customTextFieldStyle(txtCode, text: "Module Code")
         
         self.levelPickerView.backgroundColor = UIColor.clearColor()
         self.levelPickerView.delegate = self
-        self.levelPickerView.dataSource = self
+      //  self.levelPickerView.dataSource = self
         
         self.btnSave.backgroundColor = UIColor.clearColor()
         self.btnSave.backgroundColor = UIColor.clearColor()
         self.btnSave.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
         self.btnSave.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
         
-        self.btnAddCoursework.backgroundColor = UIColor.clearColor()
-        self.btnAddCoursework.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
-        self.btnAddCoursework.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
+     //   self.btnAddCoursework.backgroundColor = UIColor.clearColor()
+      // self.btnAddCoursework.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
+      //  self.btnAddCoursework.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
         
         selectedLevel = "Level 4"
-        
-      
-        
-        self.configureView()
+
     }
-    
-    
+ 
+ 
+ 
     func customTextFieldStyle(textField: UITextField, text: String) {
         
         textField.backgroundColor = UIColor.clearColor()
@@ -89,7 +60,8 @@ class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         levelPickerView.layer.borderColor = UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00).CGColor
         levelPickerView.layer.cornerRadius = 8
     }
-    
+
+ 
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -114,37 +86,11 @@ class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         selectedLevel = levels[row]
         
     }
-    
-    
-    @IBAction func btnSave(sender: MyButton) {
-        
-        if txtModule.text != "" {
-            
-            if dataController.fetchModules().contains(txtModule.text!){
-                print("Already exist")
 
-            }else{
-               dataController.addModule(txtModule.text!, code: txtModule.text!, level: selectedLevel)
-            
+    
 
-            }
-            
-            modules = dataController.fetchAll()
-            table?.reloadData()
-            
-        }else{
-            print("Empty")
-        }
-        
-    }
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-}
 
+}
